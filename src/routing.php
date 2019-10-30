e9a7f2597e42a4fd70df81f5dbb96910eec252fd
Guillaume HARARI <guillaume.harari@wildcodeschool.fr>
2019-10-30 10:20:18 +0100

    Enable cors for PUT and DELETE

--- src/routing.php
+++ src/routing.php
@@ -9,15 +9,17 @@
  * @link     https://github.com/WildCodeSchool/simple-mvc
  */
 
-$routeParts = explode('/', ltrim($_SERVER['REQUEST_URI'], '/') ?: HOME_PAGE);
-$controller = 'App\Controller\\' . ucfirst($routeParts[0] ?? '') . 'Controller';
-$method = $routeParts[1] ?? '';
-$vars = array_slice($routeParts, 2);
+if ($_SERVER['REQUEST_METHOD'] != 'OPTIONS') {
+    $routeParts = explode('/', ltrim($_SERVER['REQUEST_URI'], '/') ?: HOME_PAGE);
+    $controller = 'App\Controller\\' . ucfirst($routeParts[0] ?? '') . 'Controller';
+    $method = $routeParts[1] ?? '';
+    $vars = array_slice($routeParts, 2);
 
-if (class_exists($controller) && method_exists(new $controller(), $method)) {
-    echo call_user_func_array([new $controller(), $method], $vars);
-} else {
-    header("HTTP/1.0 404 Not Found");
-    echo '404 - Page not found';
-    exit();
+    if (class_exists($controller) && method_exists(new $controller(), $method)) {
+        echo call_user_func_array([new $controller(), $method], $vars);
+    } else {
+        header("HTTP/1.0 404 Not Found");
+        echo '404 - Page not found';
+        exit();
+    }
 }
